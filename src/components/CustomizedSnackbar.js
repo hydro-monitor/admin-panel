a3c743551ca33c31e769d12f723780c36198e496
Manuel Porto <manu.porto94@gmail.com>
2020-03-25 00:12:35 -0300

    Cretae CustomizedSnackbar.js

---
+++ src/components/CustomizedSnackbar.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+export default function CustomizedSnackbar({ props, setSnackbarData }) {
+  const [snackbarState, setSnackbarState] = useState(props);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSnackbarData({ ...snackbarState, open: false });
+  };
+
+  useEffect(() => {
+    console.log("snackbar updated");
+    setSnackbarState(props);
+  }, [props]);
+
+  return (
+    <Snackbar
+      open={snackbarState.open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+    >
+      <Alert onClose={handleClose} severity={snackbarState.severity}>
+        {snackbarState.message}
+      </Alert>
+    </Snackbar>
+  );
+}
