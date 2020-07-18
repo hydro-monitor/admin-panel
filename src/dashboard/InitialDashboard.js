b49030eeaa4b814b20436dd4fbfc375f92ef718d
Agustina Barbetta <agustina.barbetta@gmail.com>
2020-07-17 23:36:00 -0300

    clean old imports and vars

--- src/dashboard/InitialDashboard.js
+++ src/dashboard/InitialDashboard.js
@@ -1,19 +1,14 @@
 import React, { useState, useEffect } from "react";
-import clsx from "clsx";
 import Dashboard from "./Dashboard";
 import Grid from "@material-ui/core/Grid";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { sleep } from "../common/utils";
 import EmptyMeasurement from "./EmptyMeasurement";
 import Measurement from "./Measurement";
-import { useStyles } from "./dashboardStyles";
 import { handleErrors } from "../common/server";
-import { makeStyles } from "@material-ui/core/styles";
 import { WEB_API } from "../common/constants";
 
 function MeasurementList({ nodes }) {
-  const classes = useStyles();
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
   let measurements = [];
   Object.keys(nodes).forEach(id => {
     if (nodes[id]) {
@@ -69,13 +64,6 @@ export default function InitialDashboard(props) {
       </Dashboard>
     );
   }
-
-  const useStyles = makeStyles(theme => ({
-    backdrop: {
-      zIndex: theme.zIndex.drawer + 1
-    }
-  }));
-  const classes = useStyles();
 
   function renderLoading() {
     return (
