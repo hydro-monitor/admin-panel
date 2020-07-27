88ff2faee89cc5b605d036c8623e7a1056dce2d6
Agustina Barbetta <agustina.barbetta@gmail.com>
2020-07-27 17:25:45 -0300

    Dockerfile, compose and make

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,4 @@
-export const WEB_API = "http://localhost:8080"; //"http://antiguos.fi.uba.ar:443";
+export const WEB_API = "panel-api-proxy";
 export const NODES_API = WEB_API + "/api/nodes";
 export const USERS_API = WEB_API + "/api/users";
 export const SESSION_API = WEB_API + "/api/session";
