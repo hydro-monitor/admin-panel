001c502633def515664496f0aba0564b325617c4
Agustina Barbetta <agustina.barbetta@gmail.com>
2020-10-10 20:54:46 -0300

    copy pass

---
+++ src/components/CopiableTextField.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+
+const styles = (theme) => ({
+  textField: {
+    width: "100%",
+  },
+  button: {
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
+});
+
+function CopiableTextField(props) {
+  const { classes, value, label } = props;
+
+  const handleClick = () => {
+    document.execCommand(value);
+  };
+
+  return (
+    <TextField
+      value={value}
+      label={label}
+      autoFocus
+      fullWidth
+      disabled
+      className={classes.textField}
+      InputProps={{
+        endAdornment: (
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            className={classes.button}
+            disableRipple
+          >
+            <InputAdornment position="end">
+              <AssignmentIcon />
+            </InputAdornment>
+          </IconButton>
+        ),
+      }}
+    />
+  );
+}
+
+export default withStyles(styles)(CopiableTextField);
