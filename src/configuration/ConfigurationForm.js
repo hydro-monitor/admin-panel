970b4b6a23eed9c1881d182192664686067f457b
Agustina Barbetta <agustina.barbetta@gmail.com>
2020-11-09 11:51:14 -0300

    import useEffect

--- src/configuration/ConfigurationForm.js
+++ src/configuration/ConfigurationForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
